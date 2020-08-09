249991f78c3911057fd4531e63180a44d54ca91c
deepereye <109770896+deepereye@users.noreply.github.com>
2020-08-09 23:01:16 +0000

    ,,

--- examples/dodge-the-creeps/rust/src/hud.rs
+++ examples/dodge-the-creeps/rust/src/hud.rs
@@ -14,4 +14,16 @@ impl Hud {
     fn start_game();
 
     #[func]
-    pu
+    pub fn show_message(&self, text: GodotString) {
+        let mut message_label = self.base.get_node_as::<Label>("MessageLabel");
+        message_label.set_text(text);
+        message_label.show();
+
+        let mut timer = self.base.get_node_as::<Timer>("MessageTimer");
+        timer.start(0.0);
+    }
+
+    pub fn show_game_over(&self) {
+        self.show_message("Game Over".into());
+
+        let mut message_label = self.base
