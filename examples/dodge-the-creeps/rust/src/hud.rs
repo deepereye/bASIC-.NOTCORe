42a2b4c6f05ec782dbb4df866ab8366c8e040b67
deepereye <109770896+deepereye@users.noreply.github.com>
2020-08-12 14:45:42 +0000

    isdn/gigaset: ratelimit CAPI message dumps

--- examples/dodge-the-creeps/rust/src/hud.rs
+++ examples/dodge-the-creeps/rust/src/hud.rs
@@ -49,4 +49,20 @@ impl Hud {
         // Note: this works only because `start_game` is a deferred signal.
         // This method keeps a &mut Hud, and start_game calls Main::new_game(), which itself accesses this Hud
         // instance through Gd<Hud>::bind_mut(). It will try creating a 2nd &mut reference, and thus panic.
-        // Deferring the signal is one
+        // Deferring the signal is one option to work around it.
+        self.base.emit_signal("start_game".into(), &[]);
+    }
+
+    #[func]
+    fn on_message_timer_timeout(&self) {
+        let mut message_label = self.base.get_node_as::<Label>("MessageLabel");
+        message_label.hide()
+    }
+}
+
+#[godot_api]
+impl GodotExt for Hud {
+    fn init(base: Base<Self::Base>) -> Self {
+        Self { base }
+    }
+}
