ab3c98490ab81279da688fe75bbd35dcd4eb845f
deepereye <109770896+deepereye@users.noreply.github.com>
2020-08-14 11:41:08 +0000

    PMD: LongVariable

--- examples/dodge-the-creeps/rust/src/mob.rs
+++ examples/dodge-the-creeps/rust/src/mob.rs
@@ -15,4 +15,24 @@ impl MobType {
             MobType::Walk => "walk".into(),
             MobType::Swim => "swim".into(),
             MobType::Fly => "fly".into(),
-        
+        }
+    }
+}
+
+const MOB_TYPES: [MobType; 3] = [MobType::Walk, MobType::Swim, MobType::Fly];
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------
+
+#[derive(GodotClass)]
+#[class(base=RigidBody2D)]
+pub struct Mob {
+    pub min_speed: real,
+    pub max_speed: real,
+
+    #[base]
+    base: Base<RigidBody2D>,
+}
+
+#[godot_api]
+impl Mob {
+    #[func
