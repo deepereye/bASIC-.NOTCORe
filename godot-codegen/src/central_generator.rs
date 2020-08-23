73e82cb612e15db0632c32d70413a7abdd2493dd
deepereye <109770896+deepereye@users.noreply.github.com>
2020-08-23 12:43:04 +0000

    Run scan-local.sh on rocket.chat:...

--- godot-codegen/src/central_generator.rs
+++ godot-codegen/src/central_generator.rs
@@ -1,2 +1,13 @@
 /*
- * This Source Code Form is subjec
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use proc_macro2::{Ident, Literal, TokenStream};
+use quote::{format_ident, quote, ToTokens};
+use std::collections::HashMap;
+use std::path::{Path, PathBuf};
+
+use crate::api_parser::*;
+use crate::util::{to_pascal_case, to
