d51acd6fb9f407618d4c6691433f26a954e61331
deepereye <109770896+deepereye@users.noreply.github.com>
2020-08-24 13:13:15 +0000

    New card scripts

--- godot-codegen/src/central_generator.rs
+++ godot-codegen/src/central_generator.rs
@@ -10,4 +10,15 @@ use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 
 use crate::api_parser::*;
-use crate::util::{to_pascal_case, to
+use crate::util::{to_pascal_case, to_rust_type, to_snake_case};
+use crate::{ident, util, Context};
+
+struct CentralItems {
+    opaque_types: Vec<TokenStream>,
+    variant_ty_enumerators_pascal: Vec<Ident>,
+    variant_ty_enumerators_rust: Vec<TokenStream>,
+    variant_ty_enumerators_ord: Vec<Literal>,
+    variant_op_enumerators_pascal: Vec<Ident>,
+    variant_op_enumerators_ord: Vec<Literal>,
+    variant_fn_decls: Vec<TokenStream>,
+    variant_fn_inits: Vec<
