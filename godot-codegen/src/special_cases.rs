dd56ec8757bb61f6e00374ad0d26df18da915f45
deepereye <109770896+deepereye@users.noreply.github.com>
2020-10-09 18:14:18 +0000

    Quick save

--- godot-codegen/src/special_cases.rs
+++ godot-codegen/src/special_cases.rs
@@ -9,4 +9,14 @@
 // Open design decisions:
 // * Should Godot types like Node3D have all the "obj level" methods like to_string(), get_instance_id(), etc; or should those
 //   be reserved for the Gd<T> pointer? The latter seems like a limitation. User objects also have to_string() (but not get_instance_id())
-//   through the GodotExt tr
+//   through the GodotExt trait. This could be unified.
+// * The deleted/private methods and classes deemed "dangerous" may be provided later as unsafe functions -- our safety model
+//   needs to first mature a bit.
+
+// NOTE: the methods are generally implemented on Godot types (e.g. AABB, not Aabb)
+
+use crate::TyName;
+
+#[rustfmt::skip]
+pub(crate) fn is_deleted(class_name: &TyName, godot_method_name: &str) -> bool {
+    
