3dc984a3ab47324d7b2539ed975b94ab505e95b2
deepereye <109770896+deepereye@users.noreply.github.com>
2020-10-09 16:01:15 +0000

    Pep8

--- godot-codegen/src/special_cases.rs
+++ godot-codegen/src/special_cases.rs
@@ -1,4 +1,12 @@
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+// Lists all cases in the Godot class API, where deviations are considered appropriate (e.g. for safety).
+
+// Open design decisions:
+// * Should Godot types like Node3D have all the "obj level" methods like to_string(), get_instance_id(), etc; or should those
+//   be reserved for the Gd<T> pointer? The latter seems like a limitation. User objects also have to_string() (but not get_instance_id())
+//   through the GodotExt tr
