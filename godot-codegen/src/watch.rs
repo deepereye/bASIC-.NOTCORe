41016d5d051b4cdf3166354b3a2b4743a2387846
deepereye <109770896+deepereye@users.noreply.github.com>
2020-11-01 21:17:37 +0000

    fixed issue #2

--- godot-codegen/src/watch.rs
+++ godot-codegen/src/watch.rs
@@ -1,3 +1,24 @@
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of th
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use std::fs::File;
+use std::io::BufWriter;
+use std::path::Path;
+use std::time::{Duration, Instant};
+
+use std::io::Write;
+
+pub struct StopWatch {
+    last_instant: Instant,
+    metrics: Vec<Metric>,
+    lwidth: usize,
+}
+
+impl StopWatch {
+    pub fn start() -> Self {
+        Self {
+            last_instant: Instant::now(),
+            metrics
