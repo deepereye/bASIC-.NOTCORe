5df43f372fba9817a76c825bbfa1a54940c35f83
deepereye <109770896+deepereye@users.noreply.github.com>
2020-11-07 09:29:05 +0000

    remove redundant comment

--- godot-core/build.rs
+++ godot-core/build.rs
@@ -1,3 +1,17 @@
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use std::path::Path;
+
+fn main() {
+    let gen_path = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/src/gen/"));
+
+    if gen_path.exists() {
+        std::fs::remove_dir_all(gen_path).unwrap_or_else(|e| panic!("failed to delete dir: {e}"));
+    }
+
+    godot_codegen::generate_core_files(gen_path);
+}
---
+++ godot-core/src/bind.rs
@@ -0,0 +1,2 @@
+/*
+ * This Source Code Form is subject to the ter
