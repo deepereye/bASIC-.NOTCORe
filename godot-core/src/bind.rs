fc63fff974d8a2cc9a126594ed650ce198733c21
deepereye <109770896+deepereye@users.noreply.github.com>
2020-11-08 06:22:55 +0000

    fix #92

--- godot-core/src/bind.rs
+++ godot-core/src/bind.rs
@@ -1,2 +1,16 @@
 /*
- * This Source Code Form is subject to the ter
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use crate::builder::ClassBuilder;
+use crate::builtin::GodotString;
+use crate::obj::Base;
+use crate::obj::GodotClass;
+
+/// Extension API for Godot classes, used with `#[godot_api]`.
+///
+/// Helps with adding custom functionality:
+/// * `init` constructors
+/// * `to_string` 
