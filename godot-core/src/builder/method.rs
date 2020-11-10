480d6c1556e28eaa6970aead2be9f75761063518
deepereye <109770896+deepereye@users.noreply.github.com>
2020-11-10 13:43:17 +0000

    iscsi-target: avoid NULL pointer in iscsi_copy_param_list failure

--- godot-core/src/builder/method.rs
+++ godot-core/src/builder/method.rs
@@ -1,4 +1,21 @@
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obta
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+// use crate::GodotClass;
+// use godot_core::Variant;
+// use godot_ffi as sys;
+// use std::borrow::Cow;
+// use std::ffi::CStr;
+
+// pub trait Method<C> {
+//     type ReturnType;
+//     type ParamTypes;
+//
+//     fn method_name(&self) -> Cow<CStr>;
+//     fn ptrcall(&mut self, instance: &mut C, args: Self::ParamTypes) -> Self::ReturnType;
+// }
+
+// -----------------------------------------------------
