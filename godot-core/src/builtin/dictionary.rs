3cba81a487e2c34d506e53006d4d3d8de3be600b
deepereye <109770896+deepereye@users.noreply.github.com>
2021-01-01 14:01:04 +0000

    3.7.2

--- godot-core/src/builtin/dictionary.rs
+++ godot-core/src/builtin/dictionary.rs
@@ -1,2 +1,15 @@
 /*
- * This Source Code Form is sub
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use godot_ffi as sys;
+
+use crate::builtin::{inner, FromVariant, ToVariant, Variant};
+use crate::obj::Share;
+use std::fmt;
+use std::marker::PhantomData;
+use std::ptr::addr_of_mut;
+use sys::types::OpaqueDictionary;
+use sys::{ffi_methods, interface_fn, Godo
