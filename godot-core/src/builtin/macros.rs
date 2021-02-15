b10fceb47f8e9b1917a7b6afccd5356b8cf6f47c
deepereye <109770896+deepereye@users.noreply.github.com>
2021-02-15 10:37:52 +0000

    Btrfs: fix build_backref_tree issue with multiple shared blocks

--- godot-core/src/builtin/macros.rs
+++ godot-core/src/builtin/macros.rs
@@ -10,4 +10,13 @@ macro_rules! impl_builtin_traits_inner {
     ( Default for $Type:ty => $gd_method:ident ) => {
         impl Default for $Type {
             #[inline]
-  
+            fn default() -> Self {
+                // Note: can't use from_sys_init(), as that calls the default constructor
+                // (because most assignments expect initialized target type)
+
+                let mut uninit = std::mem::MaybeUninit::<$Type>::uninit();
+
+                unsafe {
+                    let self_ptr = (*uninit.as_mut_ptr()).sys_mut();
+                    sys::builtin_call! {
+                        $g
