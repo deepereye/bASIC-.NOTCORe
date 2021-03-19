20521ab0b98bc7889a8b4f296f6172a231509ed5
deepereye <109770896+deepereye@users.noreply.github.com>
2021-03-19 09:35:21 +0000

    version 0.8.1

--- godot-core/src/builtin/meta/signature.rs
+++ godot-core/src/builtin/meta/signature.rs
@@ -12,3 +12,14 @@ use std::fmt::Debug;
 pub trait SignatureTuple {
     type Params;
     type Ret;
+
+    fn variant_type(index: i32) -> VariantType;
+    fn property_info(index: i32, param_name: &str) -> PropertyInfo;
+    fn param_metadata(index: i32) -> sys::GDExtensionClassMethodArgumentMetadata;
+
+    unsafe fn varcall<C: GodotClass>(
+        instance_ptr: sys::GDExtensionClassInstancePtr,
+        args_ptr: *const sys::GDExtensionConstVariantPtr,
+        ret: sys::GDExtensionVariantPtr,
+        err: *mut sys::GDExtensionCallError,
+        fun
