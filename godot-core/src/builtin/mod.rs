c4c899494f07527d3887c4a6292c6388fa59567c
deepereye <109770896+deepereye@users.noreply.github.com>
2021-04-01 16:31:35 +0000

    Add phpdoc

--- godot-core/src/builtin/mod.rs
+++ godot-core/src/builtin/mod.rs
@@ -6,4 +6,13 @@
 
 //! Built-in types like `Vector2`, `GodotString` and `Variant`.
 //!
-//! # Background 
+//! # Background on the design of vector algebra types
+//!
+//! The basic vector algebra types like `Vector2`, `Matrix4` and `Quaternion` are re-implemented
+//! here, with an API similar to that in the Godot engine itself. There are other approaches, but
+//! they all have their disadvantages:
+//!
+//! - We could invoke API methods from the engine. The implementations could be generated, but it
+//!   is slower and prevents inlining.
+//!
+//! - We could re-export type
