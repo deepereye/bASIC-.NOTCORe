d2f911c160e91634f9e2c58d4757b4774b69cfa7
deepereye <109770896+deepereye@users.noreply.github.com>
2021-05-24 06:39:06 +0000

    ready for testing

--- godot-core/src/builtin/others.rs
+++ godot-core/src/builtin/others.rs
@@ -1,3 +1,15 @@
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with t
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+// Stub for various other built-in classes, which are currently incomplete, but whose types
+// are required for codegen
+use crate::builtin::{inner, StringName, Vector2};
+use crate::obj::{Gd, GodotClass};
+use godot_ffi as sys;
+use sys::{ffi_methods, GodotFfi};
+
+// TODO: Swap more inner math types with glam types
+// Note: ordered by enu
