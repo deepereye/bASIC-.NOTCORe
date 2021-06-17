4ae0159b673077b2893808866052084deea16ccd
deepereye <109770896+deepereye@users.noreply.github.com>
2021-06-17 21:21:27 +0000

    Refactor a little

--- godot-core/src/builtin/transform2d.rs
+++ godot-core/src/builtin/transform2d.rs
@@ -6,4 +6,19 @@
 use std::{fmt::Display, ops::*};
 
 use godot_ffi as sys;
-use sys::{ffi_methods, Go
+use sys::{ffi_methods, GodotFfi};
+
+use super::glam_helpers::{GlamConv, GlamType};
+use super::{math::*, Vector2};
+
+use super::real_consts::PI;
+use super::{real, RAffine2, RMat2};
+
+/// Affine 2D transform (2x3 matrix).
+///
+/// Represents transformations such as translation, rotation, or scaling.
+///
+/// Expressed as a 2x3 matrix, this transform consists of a two column vectors
+/// `a` and `b` representing the basis of the transform, as well as the origin:
+/// ```text
+/// [ a.
