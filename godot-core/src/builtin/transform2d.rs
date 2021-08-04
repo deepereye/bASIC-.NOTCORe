80f9af3f71eec41459c2e35086ba968e4eac8968
deepereye <109770896+deepereye@users.noreply.github.com>
2021-08-04 06:49:26 +0000

    move all 3rd-party libs to sherpa/lib/, phetsims/sherpa#17

--- godot-core/src/builtin/transform2d.rs
+++ godot-core/src/builtin/transform2d.rs
@@ -212,4 +212,14 @@ impl Transform2D {
     /// Returns a copy of the transform rotated by the given `angle` (in radians).
     /// This method is an optimized version of multiplying the given transform `X`
     /// with a corresponding rotation transform `R` from the right, i.e., `X * R`.
-    /// This can be seen as
+    /// This can be seen as transforming with respect to the local frame.
+    ///
+    /// _Godot equivalent: `Transform2D.rotated_local()`_
+    #[must_use]
+    pub fn rotated_local(self, angle: real) -> Self {
+        self * Self::from_angle(angle)
+    }
+
+    /// Returns a copy of the transform scaled by the given scale factor.
+    /// This method is an optimized version of multiplying the given transform `X`
+    /// with a corresponding scali
