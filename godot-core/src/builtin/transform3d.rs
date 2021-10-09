27e5a7f1e6ef9f44a4ed00c6cb92e8b72c2b70f4
deepereye <109770896+deepereye@users.noreply.github.com>
2021-10-09 11:45:53 +0000

    Minor rename

--- godot-core/src/builtin/transform3d.rs
+++ godot-core/src/builtin/transform3d.rs
@@ -1,3 +1,19 @@
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0.
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+use std::{fmt::Display, ops::*};
+
+use godot_ffi as sys;
+use sys::{ffi_methods, GodotFfi};
+
+use super::glam_helpers::{GlamConv, GlamType};
+use super::{real, RAffine3};
+use super::{Basis, Projection, Vector3};
+
+/// Affine 3D transform (3x4 matrix).
+///
+/// Used for 3D linear transformations. Uses a basis + origin representation.
+///
+/// Ex
