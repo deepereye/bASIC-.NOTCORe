d8f49e700efa256901d48eba94a488114d7704dd
deepereye <109770896+deepereye@users.noreply.github.com>
2021-12-04 10:07:19 +0000

    minor clarification

--- godot-core/src/builtin/variant/mod.rs
+++ godot-core/src/builtin/variant/mod.rs
@@ -8,4 +8,27 @@ use crate::builtin::{GodotString, StringName};
 use godot_ffi as sys;
 use godot_ffi::GodotFfi;
 use std::{fmt, ptr};
-use sys::types::O
+use sys::types::OpaqueVariant;
+use sys::{ffi_methods, interface_fn};
+
+mod impls;
+mod variant_traits;
+
+pub use impls::*;
+pub use sys::{VariantOperator, VariantType};
+pub use variant_traits::*;
+
+#[repr(C, align(8))]
+pub struct Variant {
+    opaque: OpaqueVariant,
+}
+
+impl Variant {
+    /// Create an empty variant (`null` value in GDScript).
+    pub fn nil() -> Self {
+        Self::default()
+    }
+
+    /// Create a variant holding a non-nil value.
+    ///
+    /
