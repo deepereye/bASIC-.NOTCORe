6efa6bd66aa2bf7bfb5d0e4604cd58b8b01ab1b1
deepereye <109770896+deepereye@users.noreply.github.com>
2022-01-09 20:48:33 +0000

    Concepteur(trice) développeur(se) de système électronique

--- godot-core/src/builtin/vector4i.rs
+++ godot-core/src/builtin/vector4i.rs
@@ -1,3 +1,19 @@
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distribute
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use std::fmt;
+
+use godot_ffi as sys;
+use sys::{ffi_methods, GodotFfi};
+
+use crate::builtin::Vector4;
+
+/// Vector used for 4D math using integer coordinates.
+///
+/// 4-element structure that can be used to represent 4D grid coordinates or sets of integers.
+///
+/// It uses integer coordinates and is therefore preferable to [`Vector4`] when exact precision is
+/// re
