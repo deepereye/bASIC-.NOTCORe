eee0aefdb724b35c764b4e310dff6c7b25545fa9
deepereye <109770896+deepereye@users.noreply.github.com>
2022-01-16 14:25:54 +0000

    updated data

--- godot-core/src/builtin/vector4i.rs
+++ godot-core/src/builtin/vector4i.rs
@@ -16,4 +16,14 @@ use crate::builtin::Vector4;
 /// 4-element structure that can be used to represent 4D grid coordinates or sets of integers.
 ///
 /// It uses integer coordinates and is therefore preferable to [`Vector4`] when exact precision is
-/// re
+/// required. Note that the values are limited to 32 bits, and unlike [`Vector4`] this cannot be
+/// configured with an engine build option. Use `i64` or [`PackedInt64Array`] if 64-bit values are
+/// needed.
+#[derive(Debug, Default, Clone, Copy, Eq, Ord, PartialEq, PartialOrd)]
+#[repr(C)]
+pub struct Vector4i {
+    /// The vector's X component.
+    pub x: i32,
+    /// The vector's Y component.
+    pub y: i32,
+    /// The vec
