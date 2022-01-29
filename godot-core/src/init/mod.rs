eed9661bd4bc0648a0d1f6c8f2ace94661550274
deepereye <109770896+deepereye@users.noreply.github.com>
2022-01-29 20:55:58 +0000

    Fix Build

--- godot-core/src/init/mod.rs
+++ godot-core/src/init/mod.rs
@@ -1,2 +1,13 @@
 /*
- * This Source Code Form is subject to th
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use godot_ffi as sys;
+use std::collections::btree_map::BTreeMap;
+
+#[doc(hidden)]
+// TODO consider body safe despite unsafe function, and explicitly mark unsafe {} locations
+pub unsafe fn __gdext_load_library<E: ExtensionLibrary>(
+    interface: *const sys::GDExtensionInter
