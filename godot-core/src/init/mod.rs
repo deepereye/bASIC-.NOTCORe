58f6a77901f86b675d77503504d55f5a2f6de828
deepereye <109770896+deepereye@users.noreply.github.com>
2022-02-11 15:55:42 +0000

    Changed license to LGPL

--- godot-core/src/init/mod.rs
+++ godot-core/src/init/mod.rs
@@ -10,4 +10,16 @@ use std::collections::btree_map::BTreeMap;
 #[doc(hidden)]
 // TODO consider body safe despite unsafe function, and explicitly mark unsafe {} locations
 pub unsafe fn __gdext_load_library<E: ExtensionLibrary>(
-    interface: *const sys::GDExtensionInter
+    interface: *const sys::GDExtensionInterface,
+    library: sys::GDExtensionClassLibraryPtr,
+    init: *mut sys::GDExtensionInitialization,
+) -> sys::GDExtensionBool {
+    let init_code = || {
+        sys::initialize(interface, library);
+
+        let mut handle = InitHandle::new();
+
+        let success = E::load_library(&mut handle);
+        // No early exit, unclear if Godot still requires output parameters to be set
+
+        let godot_init_params 
