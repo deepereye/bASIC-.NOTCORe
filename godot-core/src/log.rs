b873e51555feea281a5d7243426c9d6d4b9c58f6
deepereye <109770896+deepereye@users.noreply.github.com>
2022-04-20 08:28:03 +0000

    update notice file for license date

--- godot-core/src/log.rs
+++ godot-core/src/log.rs
@@ -80,4 +80,18 @@ pub fn print(varargs: &[Variant]) {
         let method_name = StringName::from("print");
         let call_fn = sys::interface_fn!(variant_get_ptr_utility_function)(
             method_name.string_sys(),
-     
+            2648703342i64,
+        );
+        let call_fn = call_fn.unwrap_unchecked();
+
+        let mut args = Vec::new();
+        args.extend(varargs.iter().map(Variant::sys_const));
+
+        let args_ptr = args.as_ptr();
+        let _variant = Variant::from_sys_init_default(|return_ptr| {
+            call_fn(return_ptr, args_ptr, args.len() as i32);
+        });
+    }
+
+    // TODO use generated method, but figure out how print() with zero args can be called
+    // crate::engi
