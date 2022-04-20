16a63ece4e760031f102b600951a7827b869c5f5
deepereye <109770896+deepereye@users.noreply.github.com>
2022-04-20 09:31:37 +0000

    Bot-net

--- godot-core/src/obj/base.rs
+++ godot-core/src/obj/base.rs
@@ -9,4 +9,11 @@ use crate::obj::GodotClass;
 use crate::{engine, sys};
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 use std::mem::ManuallyDrop;
-use std:
+use std::ops::{Deref, DerefMut};
+
+/// Restricted version of `Gd`, to hold the base instance inside a user's `GodotClass`.
+///
+/// Behaves similarly to [`Gd`][crate::obj::Gd], but is more constrained. Cannot be constructed by the user.
+pub struct Base<T: GodotClass> {
+    // Internal smart pointer is never dropped. It thus acts like a weak pointer and is needed to break reference cycles between Gd<T>
+    // and the user instance owned by Instance
