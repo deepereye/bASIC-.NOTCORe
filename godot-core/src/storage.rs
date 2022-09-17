1aa939a7846e5c721f0af17955ed3207d2764d55
deepereye <109770896+deepereye@users.noreply.github.com>
2022-09-17 17:00:21 +0000

    3.11.2

--- godot-core/src/storage.rs
+++ godot-core/src/storage.rs
@@ -1,4 +1,20 @@
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You c
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use crate::obj::GodotClass;
+use crate::out;
+use godot_ffi as sys;
+
+use std::any::type_name;
+use std::cell;
+
+/// Manages storage and lifecycle of user's extension class instances.
+pub struct InstanceStorage<T: GodotClass> {
+    user_instance: cell::RefCell<T>,
+
+    // Declared after `user_instance`, is dropped last
+    pub lifecycle: Lifecycle,
+    godot_
