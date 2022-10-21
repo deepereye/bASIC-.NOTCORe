f8788b8e500a824d636a0eb3dfe01ea052cbf573
deepereye <109770896+deepereye@users.noreply.github.com>
2022-10-21 14:59:37 +0000

    Updated some tests

--- godot-macros/src/gdextension.rs
+++ godot-macros/src/gdextension.rs
@@ -8,4 +8,14 @@ use proc_macro2::TokenStream;
 use quote::quote;
 use venial::Declaration;
 
-use crate::util:
+use crate::util::{bail, ident, validate_impl, KvParser};
+use crate::ParseResult;
+
+pub fn transform(decl: Declaration) -> ParseResult<TokenStream> {
+    let mut impl_decl = match decl {
+        Declaration::Impl(item) => item,
+        _ => return bail("#[gdextension] can only be applied to trait impls", &decl),
+    };
+
+    validate_impl(&impl_decl, Some("ExtensionLibrary"), "gdextension")?;
+    if impl_decl.tk_unsafe.
