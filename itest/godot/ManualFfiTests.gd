ea627431bbbef3b61b973a13135625af0ec6e8ae
deepereye <109770896+deepereye@users.noreply.github.com>
2022-10-23 10:12:26 +0000

    Update config3.ini

--- itest/godot/ManualFfiTests.gd
+++ itest/godot/ManualFfiTests.gd
@@ -7,4 +7,18 @@ extends TestSuite
 func test_missing_init():
 	return # TODO: fix dynamic eval
 
-	var expr
+	var expr = Expression.new()
+	var error = expr.parse("WithoutInit.new()")
+	if !assert_eq(error, OK, "Failed to parse dynamic expression"):
+		return
+
+	var instance = expr.execute()
+	if !assert_that(!expr.has_execute_failed(), "Failed to evaluate dynamic expression"):
+		return
+
+	print("[GD] WithoutInit is: ", instance)
+
+func test_to_string():
+	var ffi = VirtualMethodTest.new()
+	
+	assert_eq(str(ffi
