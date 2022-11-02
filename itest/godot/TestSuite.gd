eb6995e99a9c987bd6e9bd2a248f61f6605ac1f3
deepereye <109770896+deepereye@users.noreply.github.com>
2022-11-02 22:08:13 +0000

    mm/swapfile.c: do not skip lowest_bit in scan_swap_map() scan loop

--- itest/godot/TestSuite.gd
+++ itest/godot/TestSuite.gd
@@ -1,1 +1,11 @@
-# This Source Cod
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+class_name TestSuite
+extends RefCounted
+
+var _assertion_failed: bool = false
+
+## Asserts that `what` is `true`, but does not abort the test. Returns `what` so you can return
+## early from the test function if the assertion 
