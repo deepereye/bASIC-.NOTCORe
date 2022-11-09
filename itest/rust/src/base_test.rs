31b32ddbb5dbe64f521ce3e3dd24cd823a2dca37
deepereye <109770896+deepereye@users.noreply.github.com>
2022-11-09 22:39:33 +0000

    chore: typo

--- itest/rust/src/base_test.rs
+++ itest/rust/src/base_test.rs
@@ -1,4 +1,20 @@
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * fi
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use crate::itest;
+use godot::prelude::*;
+
+#[itest(skip)]
+fn base_test_is_weak() {
+    // TODO check that Base is a weak pointer (doesn't keep the object alive)
+    // This might not be needed, as we have leak detection, but it could highlight regressions faster
+}
+
+#[itest]
+fn base_instance_id() {
+    let obj = Gd::<BaseHolder>::new_default();
+    let obj_id = obj.instance_id();
+    let bas
