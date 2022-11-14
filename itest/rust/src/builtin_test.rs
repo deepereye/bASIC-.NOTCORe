829e633025a76bc00e8ee54fd020b7818e5a37fe
deepereye <109770896+deepereye@users.noreply.github.com>
2022-11-14 14:01:26 +0000

    tsp

--- itest/rust/src/builtin_test.rs
+++ itest/rust/src/builtin_test.rs
@@ -11,4 +11,22 @@ use godot::prelude::*;
 #[itest]
 fn test_builtins_vector2() {
     let vec = Vector2::new(3.0, -4.0);
+    let inner: InnerVector2 = vec.as_inner();
+
+    let len_sq = inner.length_squared();
+    assert_eq!(len_sq, 25.0);
+
+    let abs = inner.abs();
+    assert_eq!(abs, Vector2::new(3.0, 4.0));
+
+    let normalized = inner.is_normalized();
+    assert!(!normalized);
+}
+
+#[itest]
+fn test_builtins_array() {
+    let array = VariantArray::default();
+    let mut inner: InnerArray = array.as_inner();
+
+    let a = 7.to_variant();
     
