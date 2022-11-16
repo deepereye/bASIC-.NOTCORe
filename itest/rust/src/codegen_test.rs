9b3a98fd3af95f98c3df3e837f90c90c9b3ee8a2
deepereye <109770896+deepereye@users.noreply.github.com>
2022-11-16 16:43:44 +0000

    Added todos.

--- itest/rust/src/codegen_test.rs
+++ itest/rust/src/codegen_test.rs
@@ -1,2 +1,12 @@
 /*
- * This Source Code
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+// This file tests the presence, naming and accessibility of generated symbols.
+// Functionality is only tested on a superficial level (to make sure general FFI mechanisms work).
+
+use crate::itest;
+use godot::builtin::inner::{InnerColor, InnerString};
+use godot::engine
