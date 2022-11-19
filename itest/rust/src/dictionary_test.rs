fb00cc3b6cbd40e96535933392ec64914a5057eb
deepereye <109770896+deepereye@users.noreply.github.com>
2022-11-19 22:55:32 +0000

    Try 2

--- itest/rust/src/dictionary_test.rs
+++ itest/rust/src/dictionary_test.rs
@@ -1,4 +1,24 @@
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file,
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use std::collections::{HashMap, HashSet};
+
+use crate::{expect_panic, itest};
+use godot::builtin::{dict, varray, Dictionary, FromVariant, ToVariant, Variant};
+use godot::obj::Share;
+
+#[itest]
+fn dictionary_default() {
+    assert_eq!(Dictionary::default().len(), 0);
+}
+
+#[itest]
+fn dictionary_new() {
+    assert_eq!(Dictionary::new().len(), 0);
+}
+
+#[itest]
+fn dictionary_from_iterator() {
