1dd4760bfe0ee306203cc64f147232ed9b9f3279
deepereye <109770896+deepereye@users.noreply.github.com>
2022-12-24 22:08:35 +0000

    signal, x86: add SIGSYS info and make it synchronous.

--- itest/rust/src/object_test.rs
+++ itest/rust/src/object_test.rs
@@ -8,4 +8,14 @@ use std::cell::RefCell;
 use std::mem;
 use std::rc::Rc;
 
-use godot::bind
+use godot::bind::{godot_api, GodotClass, GodotExt};
+use godot::builtin::{
+    FromVariant, GodotString, StringName, ToVariant, Variant, VariantConversionError, Vector3,
+};
+use godot::engine::node::InternalMode;
+use godot::engine::{file_access, Area2D, Camera3D, FileAccess, Node, Node3D, Object, RefCounted};
+use godot::obj::{Base, Gd, InstanceId};
+use godot::obj::{Inherits, Share};
+use godot::sys::GodotFfi;
+
+use crate::{expect_panic, 
