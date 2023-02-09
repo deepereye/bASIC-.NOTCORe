82c20217ddd8a72d131bda0795475e05ec2b2444
deepereye <109770896+deepereye@users.noreply.github.com>
2023-02-09 07:46:28 +0000

    0.12.5

--- itest/rust/src/transform2d_test.rs
+++ itest/rust/src/transform2d_test.rs
@@ -1,3 +1,20 @@
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with 
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+use crate::itest;
+
+use godot::prelude::{inner::InnerTransform2D, *};
+use godot::private::class_macros::assert_eq_approx;
+
+const TEST_TRANSFORM: Transform2D = Transform2D::from_cols(
+    Vector2::new(1.0, 2.0),
+    Vector2::new(3.0, 4.0),
+    Vector2::new(5.0, 6.0),
+);
+
+#[itest]
+fn transform2d_equiv() {
+    let inner = InnerTransform2D::from_outer(&TEST_TRANSFORM);
+    let outer = TES
